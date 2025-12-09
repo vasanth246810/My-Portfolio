63892807c0aa9397a0431a681862dd3ef8956e2d
Vasanth S <vasanthkumar49480@gmail.com>
2025-12-09 21:45:01 +0530

    design chnages commited

--- src/component/ProjectInfo.jsx
+++ src/component/ProjectInfo.jsx
@@ -1,15 +1,15 @@
 import {FaHtml5, FaCss3Alt, FaReact, FaNodeJs, FaGithub, FaFigma} from 'react-icons/fa';
 import {SiTailwindcss, SiJavascript, SiRedux, SiExpress,SiGit,SiVercel, SiPostman, SiDjango,SiMysql  } from 'react-icons/si';
-import Profile from '../assets/Profile2.png';
+import Eventify from '../assets/Eventify.png';
 import { BsBootstrap } from "react-icons/bs";
-import preview from '../assets/preview.webp';
+import Tomato from '../assets/Tomato.png';
 
     
 export const ProjectInfo=[
         {
             title:"Eventifyy",
             description:"A full-stack event management and ticket booking app built with React and Django. It features secure user authentication for login and registration, event discovery with browse and search capabilities, and real-time ticket booking with seat availability tracking.",
-            image:Profile,
+            image:Eventify,
             Link:"https://eventify-event-management-system.vercel.app/",
             GithubLink:"https://github.com/vasanth246810/Eventify-Event-Management-System",
             techStack:[{name:"ReactJs",icon:FaReact,color:"#61DAFB"},
@@ -23,7 +23,7 @@ export const ProjectInfo=[
         {
             title:"Food Project",
             description:"A full-stack Food ordering app built with React and Django. It features secure user authentication for login and registration, event discovery with browse and search capabilities, and real-time ticket booking with seat availability tracking.",
-            image:preview,
+            image:Tomato,
             techStack:[{name:"ReactJs",icon:FaReact,color:"#61DAFB"},
             {name:"Django",icon:SiDjango,color:"#339933"},
             {name:"Bootstrap",icon:BsBootstrap,color:"#ef1cb3ff"},
